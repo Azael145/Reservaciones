6d8509926842cfe1f1a9a25ce0fc498a476f9924
Azael145 <2201010107@undc.edu.pe>
2025-05-29 21:31:43 -0500

    Create conexion.php
    coneion

---
+++ includes/conexion.php
@@ -0,0 +1,12 @@
+$host = 'localhost';
+$db = 'nombre_base_datos';
+$user = 'usuario';
+$pass = 'contraseña';
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
